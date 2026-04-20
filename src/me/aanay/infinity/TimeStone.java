834cce6910edd648cd230e347e7dab01a48778f3
aanayd714-svg <aanayd714@gmail.com>
2026-04-20 11:17:24 +0530

    Create TimeStone.java

---
+++ src/me/aanay/infinity/TimeStone.java
@@ -0,0 +1,15 @@
+package me.aanay.infinity;
+
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.*;
+
+public class TimeStone {
+
+    public static void use(Player p) {
+        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 2));
+
+        p.getWorld().spawnParticle(Particle.PORTAL, p.getLocation(), 100);
+        p.getWorld().playSound(p.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 1, 1);
+    }
+}
