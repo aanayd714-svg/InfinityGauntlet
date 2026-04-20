35c8537d642f5b13621a5b380314dd31b4ac67e2
aanayd714-svg <aanayd714@gmail.com>
2026-04-20 11:33:10 +0530

    Update TimeStone.java

--- src/me/aanay/infinity/TimeStone.java
+++ src/me/aanay/infinity/TimeStone.java
@@ -7,7 +7,7 @@ import org.bukkit.potion.*;
 public class TimeStone {
 
     public static void use(Player p) {
-        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 2));
+        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS, 200, 2));
 
         p.getWorld().spawnParticle(Particle.PORTAL, p.getLocation(), 100);
         p.getWorld().playSound(p.getLocation(), Sound.BLOCK_BEACON_ACTIVATE, 1, 1);
